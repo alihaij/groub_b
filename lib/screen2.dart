89fc3a8815cbb3e256e291a24133d422573a459a
Ali Abdulfatah Haij <74056767+alihaij@users.noreply.github.com>
2023-07-26 14:17:51 +0300

    update file

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -59,15 +59,7 @@ class MyApp extends StatelessWidget {
                               Icons.send,
                               color: Colors.white,
                             ))
-                      ]),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Arcane',
-                        style: TextStyle(
-                          color: Colors.white,
+\
                         ),
                       ),
                     ),
